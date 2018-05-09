a90ce443773bc0f5ca0982a50723ae53dac3d0e1
Lukas Kugler <lukikugler@gmail.com>
2018-05-09 08:29:22 +0200

    start Zuul Server

--- src/test/java/de/hska/iwi/vslab/zuulserver/ZuulServerApplicationTests.java
+++ src/test/java/de/hska/iwi/vslab/zuulserver/ZuulServerApplicationTests.java
@@ -3,10 +3,12 @@ package de.hska.iwi.vslab.zuulserver;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableZuulProxy
 public class ZuulServerApplicationTests {
 
 	@Test
