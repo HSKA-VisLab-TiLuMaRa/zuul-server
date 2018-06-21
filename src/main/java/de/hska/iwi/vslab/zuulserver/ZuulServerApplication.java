2d1f279f4818293736d72d90c7f4f4dd8422fb40
Lukas Kugler <lukikugler@gmail.com>
2018-06-21 09:25:16 +0200

    zuul auth try 1

--- src/main/java/de/hska/iwi/vslab/zuulserver/ZuulServerApplication.java
+++ src/main/java/de/hska/iwi/vslab/zuulserver/ZuulServerApplication.java
@@ -3,8 +3,10 @@ package de.hska.iwi.vslab.zuulserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 @EnableZuulProxy
 public class ZuulServerApplication {
 
