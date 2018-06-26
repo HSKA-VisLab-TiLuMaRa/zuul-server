7a4429fe65a156fb356c3b16cb73d39886cfd132
Lukas Kugler <lukikugler@gmail.com>
2018-06-26 21:30:06 +0200

    enable security with node auth server

--- src/main/java/de/hska/iwi/vslab/zuulserver/ResourceServerConfiguration.java
+++ src/main/java/de/hska/iwi/vslab/zuulserver/ResourceServerConfiguration.java
@@ -1,10 +1,10 @@
 package de.hska.iwi.vslab.zuulserver;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
 
 @Configuration
 @EnableResourceServer
@@ -16,13 +16,11 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
          
 		System.out.println("zuul http config, resourceserver ");
 		 	
-		 http		
-		 	.authorizeRequests().antMatchers("/auth/oauth/authorize", "/auth/oauth/token", "/auth/oauth/confirm_access").permitAll()   // important for authorizing request why??
-	        .and()
-	        .authorizeRequests()
-			 	.antMatchers("/auth/user").authenticated()
-			 	.anyRequest().authenticated();
-    }
+		http
+		.requestMatcher(new RequestHeaderRequestMatcher("Authorization"))
+		.authorizeRequests()
+		.antMatchers("/**").authenticated();
+	}
 
 
 }
